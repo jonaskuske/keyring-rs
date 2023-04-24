ef7d9f998665f5897dd6f8f9174ce3c1fba082a1
Jonas Kuske <mail@jonaskuske.com>
2023-04-24 13:55:12 +0200

    try linux impl for android

--- src/lib.rs
+++ src/lib.rs
@@ -72,6 +72,7 @@ pub fn platform() -> Platform {
 }
 
 // Platform-specific implementations
+#[cfg_attr(target_os = "android", path = "linux.rs")]
 #[cfg_attr(target_os = "linux", path = "linux.rs")]
 #[cfg_attr(target_os = "windows", path = "windows.rs")]
 #[cfg_attr(target_os = "macos", path = "macos.rs")]
